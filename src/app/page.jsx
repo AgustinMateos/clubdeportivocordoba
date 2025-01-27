bb18002e1cdb025fe12bc590876e1c312c5212bd
agustin mateos <agustinrotondomateos@gmail.com>
2025-01-27 16:56:20 -0300

    actividades

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,8 +14,8 @@ export default function Home() {
 
       <main className="relative">
        <PrimerComponente/>
-      <Historia/>
-       {/*  <Actividades/>
+      <Actividades/>
+       {/* <Historia/> 
        <Pesca/>
        <Colonia/> */}
        {/* <Tarifas/> */}
