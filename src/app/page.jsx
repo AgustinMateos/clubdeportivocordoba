cb740597e7808b4ab48138961dfa252cc603e796
agustin mateos <agustinrotondomateos@gmail.com>
2025-01-27 17:09:40 -0300

    beneficios solo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,7 +20,7 @@ export default function Home() {
        {/* <Historia/> 
        }
        {/* <Tarifas/> */}
-       <HaceteSocio/>
+       {/* <HaceteSocio/> */}
        <Beneficios/> 
        {/* <Footer/> */}
       </main>
