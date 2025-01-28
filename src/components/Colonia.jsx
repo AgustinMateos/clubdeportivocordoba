ca86bafa6c263d0bf8bb2763ee1715f712d77c70
agustin mateos <agustinrotondomateos@gmail.com>
2025-01-28 19:13:46 -0300

    slider colonia

--- src/components/Colonia.jsx
+++ src/components/Colonia.jsx
@@ -34,93 +34,92 @@ export default function Historia() {
 
   return (
     <div className="w-full min-h-[100vh] bg-[#F2F2F2] flex justify-center">
-      <div className=" w-[80%] flex flex-col sm:flex-row items-center justify-around">
-        {/* Sección de texto con los eventos */}
-        <div className="w-[100%]  sm:w-[50%] h-auto sm:h-[700px] flex flex-col justify-between relative">
-            <div className="h-[70%] flex justify-evenly flex-col w-[100%]">
-              <h3 className="pt-4">Colonia de Vacaciones</h3>
-              <h4 className="text-[48px] font-bold leading-[54px] pt-4 w-[100%] ">
-              Un verano inolvidable en Central Córdoba
-              </h4>
-              <p className="pt-4 w-[100%] font-semibold text-[20px]">
-              ¡Se acerca la temporada de verano y nuestra colonia de vacaciones está lista para recibirte!
-              </p>
-              <p className="pt-4 w-[100%] text-[20px]">
-              En el Club Deportivo Central Córdoba, ofrecemos actividades para todas las edades en un entorno natural y seguro.
-              </p>
-              <p className="pt-4 w-[100%] text-[20px]">
-              Disfrutá de momentos únicos en familia o con amigos mientras aprovechás nuestras instalaciones y actividades diseñadas para el máximo disfrute.
-              </p>
-            </div>
-  
-            {/* Mapeo de los eventos */}
-            <div className="w-[95%] relative pl-[20px]">
-                <h5 className="pt-[50px] pb-[20px]">Beneficios</h5>
-              {eventos.map((evento, index) => (
-                <div
-                  key={index}
-                  className="flex flex-row pb-4 items-center relative"
-                >
-                  {/* Imagen del círculo */}
-                  <Image
-                    src="/circulocolonia.svg"
-                    width={20}
-                    height={20}
-                    alt="img"
-                    className="absolute left-0"
-                  />
-                  <p className="pl-8">
-                    {/* Año en negrita */}
-                     {evento.description}
-                  </p>
-                  
-                  
-                </div>
-              ))}
-            </div>
-          </div>
+  <div className="w-[90%] sm:w-[80%] flex flex-col sm:flex-row items-center justify-around">
+    {/* Sección de texto con los eventos */}
+    <div className="w-full sm:w-[50%] h-auto sm:h-[700px] flex flex-col justify-between relative">
+      <div className="h-[70%] flex justify-evenly flex-col w-full">
+        <h3 className="pt-4">Colonia de Vacaciones</h3>
+        <h4 className="text-[48px] font-bold leading-[54px] pt-4 w-full ">
+          Un verano inolvidable en Central Córdoba
+        </h4>
+        <p className="pt-4 w-full font-semibold text-[20px]">
+          ¡Se acerca la temporada de verano y nuestra colonia de vacaciones está lista para recibirte!
+        </p>
+        <p className="pt-4 w-full text-[20px]">
+          En el Club Deportivo Central Córdoba, ofrecemos actividades para todas las edades en un entorno natural y seguro.
+        </p>
+        <p className="pt-4 w-full text-[20px]">
+          Disfrutá de momentos únicos en familia o con amigos mientras aprovechás nuestras instalaciones y actividades diseñadas para el máximo disfrute.
+        </p>
+      </div>
 
-        {/* Slider */}
-        <div className="relative w-[447px] h-[592px] overflow-hidden m-[20px] sm:m-[0px]">
-          {/* Imágenes */}
+      {/* Mapeo de los eventos */}
+      <div className="w-[95%] relative pl-[20px]">
+        <h5 className="pt-[50px] pb-[20px]">Beneficios</h5>
+        {eventos.map((evento, index) => (
           <div
-            className="flex transition-transform duration-500"
-            style={{
-              transform: `translateX(-${currentImage * 100}%)`,
-            }}
+            key={index}
+            className="flex flex-row pb-4 items-center relative"
           >
-            {images.map((src, index) => (
-              <div
-                key={index}
-                className="w-[447px] h-[592px] flex-shrink-0 "
-              >
-                <Image
-                  src={src}
-                  width={447}
-                  height={592}
-                  alt={`Slide ${index + 1}`}
-                  className="object-cover w-full h-full rounded-[86px]"
-                />
-              </div>
-            ))}
+            {/* Imagen del círculo */}
+            <Image
+              src="/circulocolonia.svg"
+              width={20}
+              height={20}
+              alt="img"
+              className="absolute left-0"
+            />
+            <p className="pl-8">
+              {/* Año en negrita */}
+              {evento.description}
+            </p>
           </div>
-
-          {/* Paginación */}
-          <div className="absolute bottom-4 left-0 right-0 flex justify-center space-x-2">
-            {images.map((_, index) => (
-              <button
-                key={index}
-                className={`w-10 h-1  transition ${
-                  currentImage === index
-                    ? "bg-[#DF3737]"
-                    : "bg-[#ffff] hover:bg-[#d66767]"
-                }`}
-                onClick={() => setCurrentImage(index)}
-              ></button>
-            ))}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
+
+    {/* Slider */}
+    <div className="relative w-full sm:w-[447px] h-[592px] overflow-hidden m-[20px] sm:m-[0px]">
+      {/* Imágenes */}
+      <div
+        className="flex transition-transform duration-500"
+        style={{
+          transform: `translateX(-${currentImage * 100}%)`,
+        }}
+      >
+        {images.map((src, index) => (
+          <div
+            key={index}
+            className="w-full sm:w-[447px] h-[592px] flex-shrink-0"
+          >
+            <Image
+              src={src}
+              width={447}
+              height={592}
+              alt={`Slide ${index + 1}`}
+              className="object-cover w-full h-full rounded-[86px]"
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Paginación */}
+      <div className="absolute bottom-4 left-0 right-0 flex justify-center space-x-2">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className={`w-10 h-1 transition ${
+              currentImage === index
+                ? "bg-[#DF3737]"
+                : "bg-[#ffff] hover:bg-[#d66767]"
+            }`}
+            onClick={() => setCurrentImage(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
